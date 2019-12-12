304f6c8f6098df42d851919a1ed1fe93e3bb9f0a
Marcos <maasouza@poli.ufrj.br>
2019-12-12 09:39:55 -0300

    Change params

--- src/functions4operation.py
+++ src/functions4operation.py
@@ -2,46 +2,46 @@ from solution import *
 import random
 
 # Remoção aleatória
-def removal_1(solution, diversity_matrix, removal_rate):
+def removal_1(solution, instance, removal_rate):
     removed_items = []
     for group in solution.groups:
         number_of_removals = round(group.num_items*removal_rate)
         while number_of_removals > 0:
             item = random.choice(list(group.items))
-            group.remove_item(item, diversity_matrix[item])
+            group.remove_item(item, instance.adj_matrix[item])
             removed_items.append(item)
             number_of_removals -= 1
     return removed_items
 
-#TODO Remoção aleatória enviesada
-def removal_2(solution, diversity_matrix, removal_rate):
+#TODO  Remoção aleatória enviesada
+def removal_2(solution, instance, removal_rate):
     removed_items = []
     for group in solution.groups:
         number_of_removals = round(group.num_items*removal_rate)
         while number_of_removals > 0:
             item = solution.worst_item
-            group.remove_item(item, diversity_matrix[item])
+            group.remove_item(item, instance.adj_matrix[item])
             removed_items.append(item)
             number_of_removals -= 1
     return removed_items
 
 # Remoção gulosa pela menor contribuição
-def removal_3(solution, diversity_matrix, removal_rate):
+def removal_3(solution, instance, removal_rate):
     removed_items = []
     for group in solution.groups:
         number_of_removals = round(group.num_items*removal_rate)
         while number_of_removals > 0:
             item = solution.worst_item
-            group.remove_item(item, diversity_matrix[item])
+            group.remove_item(item, instance.adj_matrix[item])
             removed_items.append(item)
             number_of_removals -= 1
     return removed_items
 
 #TODO Remoção shaw
-def removal_3(solution, diversity_matrix, removal_rate):
+def removal_4(solution, instance, removal_rate):
     pass
 
-def insertion_1(solution, diversity_matrix, remaining_items):
+def insertion_1(solution, instance, remaining_items):
     
     solution_groups = set(range(solution.num_groups))
     remaining_items = set(remaining_items)
@@ -51,7 +51,7 @@ def insertion_1(solution, diversity_matrix, remaining_items):
         included = False
         while not included and len(solution_groups) > 0:
             group_id = random.sample(solution_groups,1)[0]
-            included = solution.groups[group_id].add_item_if_needed(item, diversity_matrix[item])
+            included = solution.groups[group_id].add_item_if_needed(item, instance.adj_matrix[item])
             if included:
                 remaining_items.remove(item)
             else:
@@ -65,6 +65,6 @@ def insertion_1(solution, diversity_matrix, remaining_items):
         included = False
         while not included:
             group_id = random.sample(solution_groups,1)[0]
-            included = solution.groups[group_id].add_item_if_viable(item, diversity_matrix[item])
+            included = solution.groups[group_id].add_item_if_viable(item, instance.adj_matrix[item])
             if included:
                 remaining_items.remove(item)
