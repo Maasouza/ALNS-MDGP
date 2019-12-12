429cdd2e9e9dd794f2c176a78ec641dda3de8a5e
DamascenoRafael <rdamasceno@poli.ufrj.br>
2019-12-12 09:42:19 -0300

    Update functions4operation.py

--- src/functions4operation.py
+++ src/functions4operation.py
@@ -1,5 +1,6 @@
 from solution import *
 import random
+import numpy as np
 
 # Remoção aleatória
 def removal_1(solution, diversity_matrix, removal_rate):
@@ -18,8 +19,24 @@ def removal_2(solution, diversity_matrix, removal_rate):
     removed_items = []
     for group in solution.groups:
         number_of_removals = round(group.num_items*removal_rate)
+        items = list(solution.items)
+
+        prob_items = []
+        for item in items:
+            prob_items.append(1.0/solution.items[item])
+        
+        sum_prob = sum(prob_items)
+        
+        for item in range(len(items)):
+            prob_items[item] /= sum_prob
+        
+        for item in np.random.choice(items, number_of_removals, p=prob_items):
+            group.remove_item(item, diversity_matrix[item])
+            removed_items.append(item)
+            
+
+
         while number_of_removals > 0:
-            item = solution.worst_item
             group.remove_item(item, diversity_matrix[item])
             removed_items.append(item)
             number_of_removals -= 1
